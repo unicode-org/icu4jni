a4ad16f017344adfccdf0b57ff5ff658855a5865
Ram Viswanadha <rviswanadha@gmail.com>
2002-11-07 23:35:23 +0000

    [ICU-2471] bump version number of ICU4JNI
    X-SVN-Rev: 10192

--- src/classes/com/ibm/icu4jni/common/ICU4JNILoader.java
+++ src/classes/com/ibm/icu4jni/common/ICU4JNILoader.java
@@ -5,8 +5,8 @@
 *******************************************************************************
 *
 * $Source: /xsrl/Nsvn/icu/icu4jni/src/classes/com/ibm/icu4jni/common/ICU4JNILoader.java,v $ 
-* $Date: 2002/08/21 20:48:28 $ 
-* $Revision: 1.4 $
+* $Date: 2002/11/07 23:35:23 $ 
+* $Revision: 1.5 $
 *
 *******************************************************************************
 */ 
@@ -20,11 +20,11 @@ public final class ICU4JNILoader {
     public static final void loadLibrary() 
             throws UnsatisfiedLinkError{
         try{
-            System.loadLibrary("ICUInterface22");
+            System.loadLibrary("ICUInterface24");
             ErrorCode.LIBRARY_LOADED = true;  
         }
         catch(UnsatisfiedLinkError e){
-            System.loadLibrary("ICUInterface22d");
+            System.loadLibrary("ICUInterface24d");
         } 
     }
    
