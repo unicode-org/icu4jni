28bdeaa806e3647298a2cc180c0b559d6c617e30
Steven R. Loomis <srl@icu-project.org>
2008-06-26 21:46:34 +0000

    [ICU-6352] 1st pass at experimental leak bug fix, and tests
    X-SVN-Rev: 24303

--- src/classes/com/ibm/icu4jni/test/converters/TestCMemoryLeak.java
+++ src/classes/com/ibm/icu4jni/test/converters/TestCMemoryLeak.java
@@ -25,15 +25,85 @@ import com.ibm.icu4jni.converters.ByteToCharConverterICU;
 import com.ibm.icu4jni.converters.CharToByteConverterICU;
 import com.ibm.icu4jni.test.TestFmwk;
 
-import com.ibm.icu4jni.charset.*;
 import com.ibm.icu4jni.converters.NativeConverter;
 import com.ibm.icu4jni.test.TestFmwk;
 
 public class TestCMemoryLeak {
-    public static void main(String[] args) {
+    static final int THREAD_COUNT = 18;
+    static final int LOOP_COUNT=470;
+    static  int nn = 0;
+
+    private static class TestThread extends Thread {
+        public void run() {
+        while(doRun == false) {
+            try {
+             Thread.sleep(500);
+//             System.err.println("oc reprintable");
+            } catch(Throwable t) {
+            }
+        }     
+        try {
+            for (int i = 0; i < LOOP_COUNT; i++) {
+            
+                ByteToCharConverterICU convto = new ByteToCharConverterICU("UTF-8");
+//                CharsetDecoder cd = cs.newDecoder();
+//                cd //= cd.replaceWith("a");
+                String str = "abc";
+                nn++;
+
+                char chars[] = new char[2048];
+                byte b[] = "string".getBytes("UTF-8");
+                int len  = convto.convert(b, 0, b.length, chars, 0, chars.length);
+
+//                if (i % 50000 == 0) {
+//                    System.out.print(".");
+//                    if(i% (50*50000) == 0) {
+//                        System.out.println(" "+i);
+//                    }
+//                    System.out.flush();
+//                }
+                }
+         } catch (Throwable t) {
+            t.printStackTrace();
+            System.out.println("FAILED- @"+this.toString());
+
+            }
+        }
+    };
+
+    public static boolean doRun = false;
+
+    static void doMultithreadTest() throws Throwable {
+        Thread th[] = new Thread[THREAD_COUNT];
+        for(int i=0;i<THREAD_COUNT;i++) {
+            th[i] = new TestThread();
+            th[i].start();
+        }
+        System.err.println(THREAD_COUNT+" threads started.");
+        doRun = true;
+        for(;;) {
+            int j = 0;
+            for(int i=0;i<THREAD_COUNT;i++) {
+                if(th[i].isAlive()) {
+                    j++;
+                }
+            }
+            System.err.println(j+" threads alive- ops: " + nn);
+            
+            if(j==0) { return; }
+            
+            Thread.sleep(2000);
+        }
+//        System.err.println("Done.");
+    }
+ 
+    public static void main(String[] args) throws Throwable {
     int i=0;
+        if(args.length<=0||rgs[0].equals("-st")) {
+            doMultithreadTest();
+            return;
+        }
         try { 
-//            for (int i = 0; i < 10000000; i++) {
             for (;;) {
             i++;
                 ByteToCharConverterICU convto = new ByteToCharConverterICU("UTF-8");
